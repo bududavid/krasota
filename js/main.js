09477e40858da2730a1bbbb610a21c1884db8bc6
bududavid <93273247+bududavid@users.noreply.github.com>
2022-04-05 17:48:37 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let bg = document.querySelector('.bg_img');
+window.addEventListener('mousemove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;  
+    bg.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
+});
+
+$(document).ready(function(){
+   $('.main-slider').slick({
+      arrows: false,
+      dots: true,
+      slidesToShow: 1,
+      easing: 'ease-in-out',
+      autoplay: true,
+      autoplaySpeed: 4000,
+      pauseOnHover: false,
+      fade: true,
+   });
+});
+
+let search_btn = document.querySelector('.header_search');
+let search_input = document.querySelector('.main-search');
+search_btn.addEventListener('click', function(){
+	search_input.classList.toggle('main-search_active');
+})
